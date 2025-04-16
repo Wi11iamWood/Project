70b7c1cd0ff0d498aae0194fc64b529d525dcc68
William Wood <willwoooood@gmail.com>
2025-04-16 17:34:20 +0300

    creating

--- Tables.sql
+++ Tables.sql
@@ -1,18 +1,29 @@
-create table Matches if NOT exists (id serial PRIMARY key,
-				      			    date date not null,
-				      				id_league ,
-				      				id_home_team ,
-				      				id_away_team ,
-				      				id_referee ,
-				      				yk_count ,
-				      				rc_count ,
-				      				fouls_count );
+create table Matches (match_id serial PRIMARY key,
+				      			    match_date date not null,
+				      				id_league int references Leagues (id),
+				      				id_home_team int,
+				      				id_away_team int,
+				      				id_referee int references Referees (id) ,
+				      				yk_count numeric,
+				      				rc_count numeric,
+				      				fouls_count int);
 
-insert into Matches values (1, 'George');
-
-
+--insert into Matches values (1, 'George');
 
 
+create table Leagues (id serial PRIMARY key,
+				      			    name varchar(50),
+									country varchar(50),
+									team_id int,
+									team_name varchar(50));
 
-select * from test1;
-				
+--insert into Leagues values (1, 'George');
+
+
+
+create table Referees  (id serial PRIMARY key,
+									first_name varchar(50),
+									last_name varchar(50),
+									country varchar(50));
+
+
